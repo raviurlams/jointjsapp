28589c9d5882b32052f574a2859e7bbd26c4433f
ravindra <rurlam@cisco.com>
2017-02-18 16:40:12 +0530

    css classes added

--- app/controllers/dashboardCtrl.js
+++ app/controllers/dashboardCtrl.js
@@ -5,12 +5,40 @@
 
     function dashboardCtrl($scope, $rootScope, appConfiguration, $state, $ajaxFactory, localStorageService) {
         $rootScope.loginId = localStorageService.get('email');
+        $scope.isCollapsePanel = true;
         if (!$rootScope.loginId) {
             $state.go(appConfiguration.signInState);
         }
-        
-        $scope.createProject = function(){
+
+        $scope.createProject = function() {
             alert('bootstrap dialog required');
+        }
+
+        $scope.removePanel = function(event) {
+            event.preventDefault();
+            var hpanel = $(event.target).closest('div.cpanel');
+            hpanel.remove();
+            $scope.isCollapsePanel = false;
+            $('.drawingArea-data').css("border-bottom", "solid #3a3d47");
+        }
+        $scope.showHidePanel = function() {
+            event.preventDefault();
+            var hpanel = $(event.target).closest('div.cpanel');
+            var icon = $(event.target).closest('i');
+            var body = hpanel.find('div.cpanel-body');
+            var footer = hpanel.find('div.cpanel-footer');
+            body.slideToggle(300);
+            footer.slideToggle(200);
+
+            // Toggle icon from up to down
+            icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+            hpanel.toggleClass('').toggleClass('cpanel-collapse');
+            setTimeout(function() {
+                hpanel.resize();
+                hpanel.find('[id^=map-]').resize();
+            }, 50);
+
+            $('.drawingArea-data').css("height", "100%");
         }
     }
 
